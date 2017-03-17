b1c7af00fcf6d24fb106d57d186c9acc82d883c7
Meillet Robin <robin.meillet@epitech.eu>
2017-03-17 22:42:30 +0100

    Colon on Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     margin: 5,
-  }
+  },
 });
 
 export default ({ counter, increment, decrement }) => (
