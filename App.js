9967f27f53957638715b556f1125d4a7c1ec5a48
kMeillet <robin.meillet@epitech.eu>
2017-03-18 02:29:40 +0100

    Redux logger

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 
-import store from './src/store';
+import createStore from './src/store';
 
 import AppWithNavigationState from './src/AppWithNavigationState';
 
 export default () => (
-  <Provider store={store}>
+  <Provider store={createStore()}>
     <AppWithNavigationState />
   </Provider>
 );
--- src/store.js
+++ src/store.js
@@ -1,16 +1,15 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import counter from './reducers/counter';
 import navigation from './reducers/navigation';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk, __DEV__ ? createLogger() : f => f)(createStore);
 
 const reducer = combineReducers({
   counter,
   navigation,
 });
 
-const store = createStoreWithMiddleware(reducer);
-
-export default store;
+export default () => createStoreWithMiddleware(reducer);
