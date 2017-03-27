6710a02606774391c89c041750395559e81c9b0a
kMeillet <robin.meillet@epitech.eu>
2017-03-27 23:25:32 +0200

    Fix Redux-Logger call on applyMiddleware & update version

--- src/store.js
+++ src/store.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
+import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import counter from './reducers/counter';
 import navigation from './reducers/navigation';
 
-const createStoreWithMiddleware = applyMiddleware(thunk, __DEV__ ? createLogger() : f => f)(createStore);
+const identity = x => x;
+
+const logger = createLogger();
+
+const createStoreWithMiddleware = applyMiddleware(thunk, __DEV__ ? logger : identity)(createStore);
 
 const reducer = combineReducers({
   counter,
